2ca60993dea7b4972d2c3997564fd621a82adfec
Marcos Gil <a22102606@alunos.ulht.pt>
2023-10-22 12:36:29 +0100

    squad properties renaming

--- src/pt/ulusofona/lp2/deisichess/Square.java
+++ src/pt/ulusofona/lp2/deisichess/Square.java
@@ -1,9 +1,13 @@
 package pt.ulusofona.lp2.deisichess;
 
 public class Square {
-
     int id;
-    int x;
-    int y;
-    
+    int coordX;
+    int coordy;
+
+    Square(int id, int coordX, int coordY){
+        this.id = id;
+        this.coordX = coordX;
+        this.coordy = coordY;
+    }
 }
