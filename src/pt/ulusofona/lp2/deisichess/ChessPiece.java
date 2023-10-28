1eafc9659cf90817be3026d0f5e5d9c40c4347d7
Marcos Gil <a22102606@alunos.ulht.pt>
2023-10-28 13:30:06 +0100

    ChessPiece class refactor

--- src/pt/ulusofona/lp2/deisichess/ChessPiece.java
+++ src/pt/ulusofona/lp2/deisichess/ChessPiece.java
@@ -1,54 +1,32 @@
 package pt.ulusofona.lp2.deisichess;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import javax.imageio.ImageIO;
-import javax.swing.*;
+import java.util.ArrayList;
 
 public class ChessPiece {
     private int ID;
-    private int teamID; // Black Pieces - team : 0 | White Pieces team : 1
+    private int teamID; // Black team: 0 | White team: 1
     private int type; // King : 0
     private String nickname;
-    private String image = null;
-    private int coordX;
-    private int coordY;
-    private boolean dead = false;
+    private String image;
+    private int currentCoordX;
+    private int currentCoordY;
+    private boolean captured;
+    private ArrayList<Integer> captureLog;
 
     public ChessPiece(int ID, int type, int teamID, String nickname) {
         this.ID = ID;
         this.type = type;
         this.teamID = teamID;
         this.nickname = nickname;
-        //BufferedImage imageFile;
-        /*try {
-            imageFile = ImageIO.read(new File("./src/images/king-" + this.ID + "-team.png"));
-            this.image = imageFile.toString();
-        }  catch (IOException e) {
-            this.image = "";
-        }*/
     }
 
-    public ChessPiece(int ID, int type, int teamID, String nickname, String image){
-        this.ID = ID;
-        this.type = type;
-        this.teamID = teamID;
-        this.nickname = nickname;
-        this.image = image;
+    public void updatePosition(int x, int y) {
+        this.currentCoordX = x;
+        this.currentCoordY = y;
     }
-
-    // region Methods
-    public void updateCoordinates(int coordX, int coordY) {
-        this.coordX = coordX;
-        this.coordY = coordY;
+    private String getLifeStatus(){
+        return this.captured ? "capturado" : "em jogo";
     }
-
-    private String getStatus(){
-        return this.dead ? "capturado" : "em jogo";
-    }
-
     public String[] getInfo(){
         return new String[] {
                 this.ID + "",
@@ -58,33 +36,30 @@ public class ChessPiece {
                 this.image
         };
     }
-
-    public String[] getInfoWithStatus(){
+    public String[] getInfoWithLifeStatusAndPosition(){
         return new String[] {
                 this.ID + "",
                 this.type + "",
                 this.teamID + "",
-                this.getStatus(),
-                this.coordX + "",
-                this.coordY + ""
+                this.getLifeStatus(),
+                this.currentCoordX + "",
+                this.currentCoordY + ""
         };
     }
-
-    public void kill(){
-        this.dead = true;
+    public void capture(){
+        this.captured = true;
+    }
+    public int getTeamID(){
+        return this.teamID;
     }
 
     @Override
     public String toString() {
         return this.ID + " | " +
-               this.type + " | " +
-               this.teamID + " | " +
-               this.nickname + " | @(" +
-               this.coordX + ", " +
-               this.coordY + ")";
+                this.type + " | " +
+                this.teamID + " | " +
+                this.nickname + " | @(" +
+                this.currentCoordX + ", " +
+                this.currentCoordY + ")";
     }
-    public int getTeamID(){
-        return this.teamID;
-    }
-    // endregion
 }
