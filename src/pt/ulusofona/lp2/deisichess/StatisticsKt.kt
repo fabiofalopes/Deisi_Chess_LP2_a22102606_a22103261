a6d1e81dc93bfe1472e9e8e0644aa8837f272bc1
Marcos Gil <a22102606@alunos.ulht.pt>
2023-12-13 19:30:52 +0000

    Create StatisticsKt.kt

---
+++ src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
@@ -0,0 +1,7 @@
+package pt.ulusofona.lp2.deisichess
+
+class StatisticsKt {
+    fun getStatsCalculator(type: StatType): (GameManager) -> ArrayList<String> {
+        return { gameManager -> ArrayList()  };
+    }
+}
