e9ea67e78dcb387ede0df9dc2ae49357a6e9a87d
Marcos Gil <a22102606@alunos.ulht.pt>
2023-12-20 03:52:35 +0000

    IOException fix

--- src/pt/ulusofona/lp2/deisichess/GameManager.java
+++ src/pt/ulusofona/lp2/deisichess/GameManager.java
@@ -2,10 +2,7 @@ package pt.ulusofona.lp2.deisichess;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.List;
 
@@ -20,85 +17,89 @@ public class GameManager {
         this.game = new Game();
     }
 
-    public void loadGame(File file) throws InvalidGameInputException, IOException {
+    public void loadGame(File file) throws InvalidGameInputException {
         if(file == null){
             return;
         }
 
-        final int COUNT_COLUMNS_COUNT = 4;
-        int countRead = 2;
-        HashMap<Integer, Piece> loadedPieces = new HashMap<>();
+        try {
+            final int COUNT_COLUMNS_COUNT = 4;
+            int countRead = 2;
+            HashMap<Integer, Piece> loadedPieces = new HashMap<>();
 
-        this.init();
+            this.init();
 
-        BufferedReader reader = new BufferedReader(new FileReader(file));
+            BufferedReader reader = new BufferedReader(new FileReader(file));
 
-        int boardDimension = Integer.parseInt(reader.readLine().trim());
-        int numberOfPieces = Integer.parseInt(reader.readLine().trim());
+            int boardDimension = Integer.parseInt(reader.readLine().trim());
+            int numberOfPieces = Integer.parseInt(reader.readLine().trim());
 
-        // read and load pieces info
-        for (int i = 0; i < numberOfPieces; i++){
-            countRead++;
+            // read and load pieces info
+            for (int i = 0; i < numberOfPieces; i++){
+                countRead++;
 
-            String pieceInfo = reader.readLine();
-            String[] pieceData = pieceInfo.split(":");
+                String pieceInfo = reader.readLine();
+                String[] pieceData = pieceInfo.split(":");
 
-            // handle invalid data format exceptions
-            if(pieceData.length < COUNT_COLUMNS_COUNT)
-            {
-                throw new InvalidGameInputException(countRead, InvalidGameInputException.getLessDataErrorDescription(
-                        COUNT_COLUMNS_COUNT, pieceData.length));
-            }
-            else if (pieceData.length > COUNT_COLUMNS_COUNT)
-            {
-                throw new InvalidGameInputException(countRead, InvalidGameInputException.getMoreDataErrorDescription(
-                        COUNT_COLUMNS_COUNT, pieceData.length));
-            }
+                // handle invalid data format exceptions
+                if(pieceData.length < COUNT_COLUMNS_COUNT)
+                {
+                    throw new InvalidGameInputException(countRead, InvalidGameInputException.getLessDataErrorDescription(
+                            COUNT_COLUMNS_COUNT, pieceData.length));
+                }
+                else if (pieceData.length > COUNT_COLUMNS_COUNT)
+                {
+                    throw new InvalidGameInputException(countRead, InvalidGameInputException.getMoreDataErrorDescription(
+                            COUNT_COLUMNS_COUNT, pieceData.length));
+                }
 
-            // create piece
-            int id = Integer.parseInt(pieceData[0].trim());
-            int typeId = Integer.parseInt(pieceData[1].trim());
-            int teamId = Integer.parseInt(pieceData[2].trim());
-            String nickname = pieceData[3].trim();
+                // create piece
+                int id = Integer.parseInt(pieceData[0].trim());
+                int typeId = Integer.parseInt(pieceData[1].trim());
+                int teamId = Integer.parseInt(pieceData[2].trim());
+                String nickname = pieceData[3].trim();
 
-            Piece piece = Piece.create(id, typeId, teamId, nickname);
-            this.game.addPiece(piece);
-            loadedPieces.put(id, piece);
-        }
-
-        // read and load board info
-        for(int row = 0; row < boardDimension; row++){
-            countRead++;
-
-            String squareInfo = reader.readLine();
-            String[] squareData = squareInfo.split(":");
-
-            // handle invalid data format exceptions
-            if(squareData.length < boardDimension)
-            {
-                throw new InvalidGameInputException(countRead, InvalidGameInputException.getLessDataErrorDescription(
-                        boardDimension, squareData.length));
-            }
-            else if (squareData.length > boardDimension)
-            {
-                throw new InvalidGameInputException(countRead, InvalidGameInputException.getMoreDataErrorDescription(
-                        boardDimension, squareData.length));
+                Piece piece = Piece.create(id, typeId, teamId, nickname);
+                this.game.addPiece(piece);
+                loadedPieces.put(id, piece);
             }
 
-            // create square
-            for(int column = 0; column < boardDimension; column++){
-                int pieceId = Integer.parseInt(squareData[column].trim());
+            // read and load board info
+            for(int row = 0; row < boardDimension; row++){
+                countRead++;
 
-                this.game.addSquare(new Square(column, row));
+                String squareInfo = reader.readLine();
+                String[] squareData = squareInfo.split(":");
 
-                if(pieceId != 0){
-                    Piece piece = loadedPieces.get(pieceId);
-                    piece.setPosition(column, row);
+                // handle invalid data format exceptions
+                if(squareData.length < boardDimension)
+                {
+                    throw new InvalidGameInputException(countRead, InvalidGameInputException.getLessDataErrorDescription(
+                            boardDimension, squareData.length));
+                }
+                else if (squareData.length > boardDimension)
+                {
+                    throw new InvalidGameInputException(countRead, InvalidGameInputException.getMoreDataErrorDescription(
+                            boardDimension, squareData.length));
+                }
+
+                // create square
+                for(int column = 0; column < boardDimension; column++){
+                    int pieceId = Integer.parseInt(squareData[column].trim());
+
+                    this.game.addSquare(new Square(column, row));
+
+                    if(pieceId != 0){
+                        Piece piece = loadedPieces.get(pieceId);
+                        piece.setPosition(column, row);
+                    }
                 }
             }
-        }
 
-        this.game.addBackup(this.game.clone());
+            this.game.addBackup(this.game.clone());
+        }
+        catch (Exception e){
+        }
     }
 
     public int getBoardSize(){
@@ -121,7 +122,7 @@ public class GameManager {
         //                 ii)  no current piece on (x,y)
         if((x0 == x1 && y0 == y1) || playingPiece == null) {
             playingTeam.incrementInvalidMoves();
-            this.game.addBackup(this.game.clone());
+            //this.game.addBackup(this.game.clone());
             return false;
         }
 
@@ -131,7 +132,7 @@ public class GameManager {
         // (invalid move): invalid move rule
         if(!validMove){
             playingTeam.incrementInvalidMoves();
-            this.game.addBackup(this.game.clone());
+            //this.game.addBackup(this.game.clone());
             return false;
         }
 
@@ -141,7 +142,7 @@ public class GameManager {
             // (invalid move) destiny piece from the same team
             if(destinyPiece.getTeamId() == playingPiece.teamId){
                 playingTeam.incrementInvalidMoves();
-                this.game.addBackup(this.game.clone());
+                //this.game.addBackup(this.game.clone());
                 return false;
             }
 
